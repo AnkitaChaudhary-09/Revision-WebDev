d1c3620dc263e086afaa12467677e5588580c9fb
AnkitaChaudhary-09 <ankitachaudhary1009@gmail.com>
2025-09-24 05:38:47 +0000

    operation & comparison

---
+++ javaScript/op.js
@@ -0,0 +1,21 @@
+//    Operations
+let value = 3;
+let negValue=-value;
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**3);
+
+// let str1 ="hlo"
+// let str2 =" ankita";
+// console.log(str1+str2);
+// console.log("2"+2);22
+// console.log(2+"2");//22
+// console.log("1"+2+2);//122
+// console.log(1+2+"2");//32
+
+console.log(+true);//1
