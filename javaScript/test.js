d33606f893c848536ad03820cdb11594b35e1bd6
AnkitaChaudhary-09 <ankitachaudhary1009@gmail.com>
2025-09-24 04:58:31 +0000

    conversion of datatypes

--- javaScript/test.js
+++ javaScript/test.js
@@ -1,1 +1,17 @@
 console.log("Ankita")
+"use strict"; // treat all JS code as new version
+//alert(3+3) // usong nodejs not browser
+
+let score =null
+console.log(typeof score);
+console.log(typeof(score));
+let val =Number(score);
+console.log(typeof val);
+console.log( val);
+//"33"=> 33
+//"33abc"=> NaN(not a number)
+//true=>1
+
+// 1=> true, 0=> false
+//"" => false
+// ank => true
