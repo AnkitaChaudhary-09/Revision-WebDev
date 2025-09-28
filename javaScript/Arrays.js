20538bf124edd34219dd876d84caa32698b6173a
AnkitaChaudhary-09 <ankitachaudhary1009@gmail.com>
2025-09-28 19:19:10 +0000

    arrays in JS

---
+++ javaScript/Arrays.js
@@ -0,0 +1,6 @@
+// these are resizeable and contain a mix datatypes.
+const arr =[1,2,3,4,"hlo"]
+console.log(arr.length);
+console.log(arr[4]);
+const arr2 =new Array(1,9,3,4,"hey")
+console.log(arr2[4]);
